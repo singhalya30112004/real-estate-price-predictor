ebcea4b861885b6a77c22ed8c12be9e234ce0ef8
Alya Singh <pulsealya@gmail.com>
2025-06-24 16:01:37 +0530

    Plotted price_per_sqft distribution with zoomed view (₹0–₹20k/sqft)

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-# Plot price distribution
-plt.figure(figsize=(10, 5))
-sns.histplot(df['price'] / 100000, bins=100, kde=True)
-plt.xlim(0, 500)  # Limit to 0–500 lakhs (₹0–₹5Cr)
-plt.title("Price Distribution (Zoomed In)")
-plt.xlabel("Price (in Lakhs)")
-plt.ylabel("Count")
-plt.tight_layout()
-plt.show()
